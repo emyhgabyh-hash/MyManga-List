bf74feb9c142052125b2056ae121fd832dbfb431
emyhgabyh-hash <emyhgabyh@gmail.com>
2026-05-07 09:13:31 -0300

    Tela de login

---
+++ lib/login_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key}); 
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+
+      appBar: AppBar(
+        title: Text(
+          "Entrar",
+          style: GoogleFonts.modak(),
+        ),
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+
+          children: [
+
+            Text(
+              "MyManga List",
+              style: GoogleFonts.modak(
+                fontSize: 36,
+                color: const Color.fromARGB(255, 81, 49, 134), 
+              ),
+            ),
+
+            const SizedBox(height: 40),
+
+            TextField(
+              decoration: const InputDecoration(
+                labelText: "Email",
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            TextField(
+              obscureText: true,
+              decoration: const InputDecoration(
+                labelText: "Senha",
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            const SizedBox(height: 30),
+
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+
+                },
+                child: const Text("Entrar"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -87,11 +88,14 @@ class HomePage extends StatelessWidget {
             SizedBox(height: 40),
 
             ElevatedButton(
-
               onPressed: () {
-
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => LoginPage(),
+                  ),
+                );
               },
-
               child: Text("Entrar"),
             ),
 
